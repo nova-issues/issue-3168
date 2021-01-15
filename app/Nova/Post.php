5d594035de5b2a01fc31e14f359a7b79bc9c92e2
Mior Muhammad Zaki <crynobone@gmail.com>
2021-01-15 22:34:27 +0800

    Reproducing code for laravel/nova-issues#3168
    Signed-off-by: Mior Muhammad Zaki <crynobone@gmail.com>

--- app/Nova/Post.php
+++ app/Nova/Post.php
@@ -3,26 +3,27 @@
 namespace App\Nova;
 
 use Illuminate\Http\Request;
-use Laravel\Nova\Fields\Gravatar;
+use Laravel\Nova\Fields\BelongsTo;
 use Laravel\Nova\Fields\ID;
-use Laravel\Nova\Fields\Password;
 use Laravel\Nova\Fields\Text;
+use Laravel\Nova\Fields\Trix;
+use Laravel\Nova\Http\Requests\NovaRequest;
 
-class User extends Resource
+class Post extends Resource
 {
     /**
      * The model the resource corresponds to.
      *
      * @var string
      */
-    public static $model = \App\Models\User::class;
+    public static $model = \App\Models\Post::class;
 
     /**
      * The single value that should be used to represent the resource when being displayed.
      *
      * @var string
      */
-    public static $title = 'name';
+    public static $title = 'id';
 
     /**
      * The columns that should be searched.
@@ -30,18 +31,8 @@ class User extends Resource
      * @var array
      */
     public static $search = [
-        'id', 'name', 'email',
+        'id',
     ];
-
-    /**
-     * Get the search result subtitle for the resource.
-     *
-     * @return string|null
-     */
-    public function subtitle()
-    {
-        return $this->email;
-    }
 
     /**
      * Get the fields displayed by the resource.
@@ -52,24 +43,10 @@ class User extends Resource
     public function fields(Request $request)
     {
         return [
-            ID::make()->sortable(),
-
-            Gravatar::make()->maxWidth(50),
-
-            Text::make('Name')
-                ->sortable()
-                ->rules('required', 'max:255'),
-
-            Text::make('Email')
-                ->sortable()
-                ->rules('required', 'email', 'max:254')
-                ->creationRules('unique:users,email')
-                ->updateRules('unique:users,email,{{resourceId}}'),
-
-            Password::make('Password')
-                ->onlyOnForms()
-                ->creationRules('required', 'string', 'min:8')
-                ->updateRules('nullable', 'string', 'min:8'),
+            ID::make(__('ID'), 'id')->sortable(),
+            BelongsTo::make('User')->searchable()->withSubtitles(),
+            Text::make('Title'),
+            Trix::make('Content')->nullable(),
         ];
     }
 
